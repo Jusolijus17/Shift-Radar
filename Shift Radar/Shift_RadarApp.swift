e4cd1c7efd643f84aa20e0350e7986c413d59349
Justin Lefrançois <justin.lefrancois@polymtl.ca>
2023-10-24 18:33:11 -0400

    Initial Commit

---
+++ Shift Radar/Shift_RadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shift_RadarApp.swift
+//  Shift Radar
+//
+//  Created by Justin Lefrançois on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct Shift_RadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
